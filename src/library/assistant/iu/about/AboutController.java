920cd0f959f724eb3014701ab527a0245f211c2d
sizwe-eddy <nkosiczwe@gmail.com>
2020-07-27 21:33:15 +0200

    #65 commit - Adding about backend functionality.

--- src/library/assistant/iu/about/AboutController.java
+++ src/library/assistant/iu/about/AboutController.java
@@ -3,35 +3,72 @@ package library.assistant.iu.about;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import library.assistant.alert.AlertMaker;
+import library.assistant.util.LibraryAssistantUtil;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AboutController implements Initializable {
 
+    private static final String LINKED_IN = "https://www.linkedin.com";
+    private static final String FACEBOOK = "https://www.facebook.com";
+    private static final String WEBSITE =   "https://www.genuinecoder.com";
+    private static final String YOUTUBE =   "https://www.youtube.com";
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        AlertMaker.showTrayMessage(String.format("Hello %s!", System.getProperty("user.name")), "Thanks for trying out Library Assistant");
+    }
+
+    private void loadWebPage(String url){
+        try {
+            Desktop.getDesktop().browse(new URI(url));
+        }catch (IOException | URISyntaxException el){
+            el.printStackTrace();
+            handleWebPageLoadException(url);
+        }
+    }
+
+    private void handleWebPageLoadException(String url){
+        WebView browser = new WebView();
+        WebEngine webEngine = browser.getEngine();
+        webEngine.load(url);
+        Stage stage = new Stage();
+        Scene scene = new Scene(new StackPane(browser));
+        stage.setScene(scene);
+        stage.setTitle("Sizwe Nkosi");
+        stage.show();
+        LibraryAssistantUtil.setStageIcon(stage);
+    }
+
     @FXML
     void loadBlog(ActionEvent event) {
-
+        loadWebPage(WEBSITE);
     }
 
     @FXML
     void loadFacebook(ActionEvent event) {
-
+        loadWebPage(FACEBOOK);
     }
 
     @FXML
     void loadLinkedIn(ActionEvent event) {
-
+        loadWebPage(LINKED_IN);
     }
 
     @FXML
     void loadYoutubeChannel(ActionEvent event) {
-
+        loadWebPage(YOUTUBE);
     }
 
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-
-    }
 }

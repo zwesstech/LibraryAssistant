8dc7974f7f8deb90fca67612f52a7ed222eb2c29
sizwe-eddy <nkosiczwe@gmail.com>
2020-07-27 18:58:05 +0200

    #63 commit - Implementing test mail.

--- src/library/assistant/iu/mail/TestMailController.java
+++ src/library/assistant/iu/mail/TestMailController.java
@@ -2,12 +2,16 @@ package library.assistant.iu.mail;
 
 import com.jfoenix.controls.JFXProgressBar;
 import com.jfoenix.controls.JFXTextField;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import library.assistant.alert.AlertMaker;
+import library.assistant.data.callback.GenericCallback;
 import library.assistant.database.MailServerInfo;
 import library.assistant.email.EmailUtil;
 import library.assistant.util.LibraryAssistantUtil;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,7 +20,7 @@ import java.util.ResourceBundle;
 
 
 
-public class TestMailController implements Initializable {
+public class TestMailController implements Initializable, GenericCallback {
 
     private static final Logger LOGGER = LogManager.getLogger(TestMailController.class.getName());
 
@@ -34,13 +38,34 @@ public class TestMailController implements Initializable {
 
     }
 
+    public void sendMailServer(MailServerInfo mailServerInfo){
+        this.mailServerInfo = mailServerInfo;
+    }
+
     @FXML
     void handleStartAction(ActionEvent event) {
         String toAddress = recipientAddressInput.getText();
         if (LibraryAssistantUtil.validateEmailAddress(toAddress)){
-
+            EmailUtil.sendTestMail(mailServerInfo, toAddress, this);
+            progressBar.setVisible(true);
+        }else {
+            AlertMaker.showErrorMessage("Failed", "Invalid email address");
         }
 
     }
 
+    @Override
+    public Object taskCompleted(Object val) {
+        LOGGER.log(Level.INFO, "Callback received from Email Sender client ()", val);
+        boolean result = (boolean) val;
+        Platform.runLater(() -> {
+            if (result){
+                AlertMaker.showSimpleAlert("Success", "Email successfully sent!");
+            }else {
+                AlertMaker.showErrorMessage("Failed", "Something went wrong");
+            }
+            progressBar.setVisible(false);
+        });
+        return true;
+    }
 }

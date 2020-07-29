8da3774e38e983858a55c35231bc9df8d21fc491
sizwe-eddy <nkosiczwe@gmail.com>
2020-07-29 19:19:38 +0200

    #71 commit - Adding Encryption.

---
+++ src/library/assistant/encryption/CipherSpec.java
@@ -0,0 +1,34 @@
+package library.assistant.encryption;
+
+import java.io.Serializable;
+import java.util.Arrays;
+
+public class CipherSpec implements Serializable {
+
+    private final byte[] key;
+    private final byte[] iv;
+
+    public CipherSpec(byte[] key, byte[] iv) {
+        this.key = key;
+        this.iv = iv;
+    }
+
+    public byte[] getKey() {
+        return key;
+    }
+
+    public byte[] getIv() {
+        return iv;
+    }
+
+    public boolean isValid(){
+        return key != null && iv != null;
+    }
+
+    @Override
+    public String toString() {
+        return "CipherSpec{" +
+                "key=" + key +
+                ", iv=" + iv + '}';
+    }
+}
---
+++ src/library/assistant/encryption/EncryptionUtil.java
@@ -0,0 +1,140 @@
+package library.assistant.encryption;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class EncryptionUtil {
+
+    private final static Logger LOGGER = LogManager.getLogger(EncryptionUtil.class.getName());
+    private final static String ENCRYPTION_ALGORITHM = "AES/CBC/PKCS5PADDING";
+    private final static String SECRET_KEY_SPEC = "AES";
+    private final static File KEY_STORE = new File("store/key.spec");
+    private final static Lock LOCK = new ReentrantLock(true);
+
+    public static String encrypt(String plainText){
+        LOCK.tryLock();
+        try {
+            CipherSpec spec = getCipherSpec();
+            if (spec == null || !spec.isValid()){
+                throw new RuntimeException("Cant load encryption");
+            }
+            return encrypt(spec.getKey(), spec.getIv(), plainText);
+        }catch (Exception ex){
+            LOGGER.log(Level.ERROR, "Encryption failure", ex);
+        }finally {
+            LOCK.unlock();
+        }
+        return null;
+    }
+
+    public static String decrypt(String cipherText){
+        LOCK.lock();
+        try {
+            CipherSpec spec = getCipherSpec();
+            if (spec == null || !spec.isValid()){
+                throw new RuntimeException("Cant load encryption");
+            }
+            return decrypt(spec.getKey(), spec.getIv(), cipherText);
+        }catch (Exception ex){
+            LOGGER.log(Level.ERROR, "Encryption failure", ex);
+        }finally {
+            LOCK.unlock();
+        }
+        return null;
+    }
+
+    private static String encrypt(byte[] key, byte[] initVector, String value) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector);
+            SecretKeySpec keySpec = new SecretKeySpec(key, SECRET_KEY_SPEC);
+            Cipher cipher = Cipher.getInstance(ENCRYPTION_ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec, iv);
+            byte[] encrypted = cipher.doFinal(value.getBytes());
+            return Base64.encodeBase64String(encrypted);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    private static String decrypt(byte[] key, byte[] initVector, String encrypted) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector);
+            SecretKeySpec keySpec = new SecretKeySpec(key, SECRET_KEY_SPEC);
+            Cipher cipher = Cipher.getInstance(ENCRYPTION_ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec, iv);
+            byte[] original = cipher.doFinal(Base64.decodeBase64(encrypted));
+            return new String(original);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void init() throws Exception{
+        CipherSpec spec = getCipherSpec();
+        if (spec == null || !spec.isValid()){
+            LOGGER.log(Level.INFO, "Preparing new cipher setup");
+            byte[] key = generateSecureKey();
+            byte[] initVector = prepareIV();
+            spec = new CipherSpec(key, initVector);
+            writeKey(spec);
+        }
+    }
+
+    private static byte[] generateSecureKey() throws NoSuchAlgorithmException {
+        KeyGenerator keyGen = KeyGenerator.getInstance(SECRET_KEY_SPEC);
+        keyGen.init(128);
+        SecretKey secretKey = keyGen.generateKey();
+        byte[] data = secretKey.getEncoded();
+        return data;
+    }
+
+    private static byte[] prepareIV() throws NoSuchAlgorithmException{
+        MessageDigest digest = MessageDigest.getInstance("SHA-512");
+        String randomVal = String.valueOf(new Random(System.currentTimeMillis()).nextLong());
+        byte[] hash = digest.digest(randomVal.getBytes(StandardCharsets.UTF_8));
+        return Arrays.copyOfRange(hash, 0, 16);
+    }
+
+    private static void writeKey(CipherSpec spec) throws Exception {
+        KEY_STORE.mkdir();
+        if (KEY_STORE.exists()){
+            LOGGER.log(Level.INFO, "Clearing existing encryption info");
+            KEY_STORE.delete();
+        }else {
+            KEY_STORE.createNewFile();
+        }
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(KEY_STORE, false))){
+            out.writeObject(spec);
+        }
+        if (KEY_STORE.exists()){
+            LOGGER.log(Level.INFO, "Added new encryption setup");
+        }
+    }
+
+    private static CipherSpec getCipherSpec() throws Exception{
+        if (KEY_STORE.exists()){
+            try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(KEY_STORE))){
+                return (CipherSpec) in.readObject();
+            }
+        }
+        return null;
+    }
+}
